2e6518ccf1c5ab1e2eed8b7439c53ea050ff3439
gajun_choi <5tmdhkfem@naver.com>
2023-09-15 22:20:15 +0900

    test

---
+++ src/main/java/com/DXsprint/dockggu/DockgguApplication.java
@@ -0,0 +1,13 @@
+package com.DXsprint.dockggu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockgguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockgguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DXsprint/dockggu/DockgguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DXsprint.dockggu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockgguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
